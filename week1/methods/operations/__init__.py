e17b37f00c686a779e3ee4fa4e8eeb49e62ad7e8
Jorge López <jorge.lopez@neosentec.com>
2018-10-10 21:40:21 +0200

    Revert "Add files via upload"
    This reverts commit 6add78dac0267bd152c07172e279ca246140c220.

--- week1/methods/operations/__init__.py
+++ week1/methods/operations/__init__.py
@@ -2,4 +2,4 @@ from .discard_geometry import instance as discard_geometry
 from .fill_holes import get_mask as fill_holes
 from .segregation import get_mask as segregation
 from .histogram_equalization import get_image as histogram_equalization
-from .morphology_operations import morphology_operations as morpho
+from .blur import mask_blurring as blur
