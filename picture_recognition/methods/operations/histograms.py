6e177a1dc1ae3835a13c85cf21fa8f4bf79e2026
Claudia Baca perez <claudiabaca.perez@e-campus.uab.cat>
2018-10-23 23:37:01 +0200

    --

--- picture_recognition/methods/operations/histograms.py
+++ picture_recognition/methods/operations/histograms.py
@@ -1,6 +1,7 @@
 import numpy as np
 from enum import Enum
 import cv2
+from cv2 import COLOR_BGR2Luv
 
 class HistogramTypes(Enum):
     HSV = 0,
@@ -9,12 +10,20 @@ class HistogramTypes(Enum):
 
 def get_histogram(im: np.array, histogramType=HistogramTypes.HSV) -> np.array:
     
-    im = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
     
     hist = []
     hist= cv2.calcHist([im], [0,1,2], None , [256], [0, 180, 0, 256, 0, 256])
     hist = cv2.normalize(hist, hist).flatten()
-
+    
+    """
+    im= cv2.cvtColor(im, cv2,COLOR_BGR2Luv)
+    hist = []
+    hist= cv2.calcHist([im], [0,1,2], None , [256], [0, 256, 0, 256, 0, 256])
+    hist = cv2.normalize(hist, hist).flatten()
+    
+    """
+    
     return hist
 
     pass
