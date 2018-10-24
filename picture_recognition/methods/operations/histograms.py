c4a164063ce5bfb249291867734ff27f470133e4
Claudia Baca perez <claudiabaca.perez@e-campus.uab.cat>
2018-10-24 17:37:46 +0200

    Blocks padding de columnas no va muy bien luego seguimos!

--- picture_recognition/methods/operations/histograms.py
+++ picture_recognition/methods/operations/histograms.py
@@ -13,7 +13,7 @@ def get_histogram(im: np.array, histogramType=HistogramTypes.HSV) -> np.array:
     im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
     
     hist = []
-    hist= cv2.calcHist([im], [0,1,2], None , [256], [0, 180, 0, 256, 0, 256])
+    hist= cv2.calcHist([im], [0,1,2], None , [bins,bins,bins], [0, 180, 0, 256, 0, 256])
     hist = cv2.normalize(hist, hist).flatten()
     
     """
