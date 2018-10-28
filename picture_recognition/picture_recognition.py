25c60c3137a25a8a8cfc67840c7fa89e0f33527c
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2018-10-28 19:12:15 +0100

    Removed method 1

--- picture_recognition/picture_recognition.py
+++ picture_recognition/picture_recognition.py
@@ -9,7 +9,7 @@ import ml_metrics as metrics
 from functional import seq
 from tabulate import tabulate
 
-from methods import AbstractMethod, method1, ycbcr_16_hellinger, ycbcr_32_correlation, hsv_16_hellinger
+from methods import AbstractMethod, ycbcr_16_hellinger, ycbcr_32_correlation, hsv_16_hellinger
 from model import Data, Picture
 
 
